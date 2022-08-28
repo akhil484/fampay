af277d7f232b818b46d11bb350292ad93afb68f2
akhil <sharma.akhil484@gmail.com>
2022-08-28 13:55:16 +0530

    Created Table to store video information

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class VideoInformation(models.Model):
+	title = models.CharField(max_length=255)
+	channelTitle = models.CharField(max_length=255)
+	description = models.CharField(max_length=512, blank=True)
+	videoId = models.CharField(max_length=512, blank=True)
+	published_on = models.CharField(max_length=255)
+	thumbnail_url = models.CharField(max_length=512, blank=True)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+	is_deleted = models.BooleanField(default=False) 
